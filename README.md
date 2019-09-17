bfacc191c9ac02b3c36474a7749d5c1ea8b76df8
suman <suman.kunwar@daliaresearch.com>
2019-09-17 09:51:36 +0200

    facebook instant games support added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-baghchal
+Baghchal
 ======
 Bagh-Chal is a strategic, two-player board game that origniated in Nepal. The game is asymmetric in that one player controls four tigers and the other player controls up to twenty goats. The tigers 'hunt' the goats while the goats attempt to block the tigers' movements. This game is also seen in southern India with a different board, but the rules are the same.
 
@@ -32,7 +32,8 @@ yarn install && yarn start
 
 ## Demos
 
-Coming soon...
+https://bagh-chal-dd292.firebaseapp.com/
+
 
 
 
@@ -49,7 +50,7 @@ yarn run test
 
 ## Contributing
 
-Feel free to pull and contribute!  If you do, please make a separate branch on your Pull Request, rather than pushing your changes to the Master.  It would also be greatly appreciated if you ran the appropriate tests before submitting the request (there are three sets, listed below).
+Feel free to pull and contribute!  If you do, please make a separate branch on your Pull Request, rather than pushing your changes to the Master.  It would also be greatly appreciated if you ran the appropriate tests before submitting the request.
 
 
 ## License
--- webpack.config.js
+++ webpack.config.js
@@ -2,80 +2,85 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
-module.exports = {
-    mode: 'development',
-    entry: ['./src/library/index.js', './src/library/scss/style.scss'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        open: true,
-        hot: true,
-        port: 5000||process.env.port,
-        writeToDisk:true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+
+module.exports = (env) => {
+    return {
+        mode: env || 'development',
+        entry: ['./src/library/index.js', './src/library/scss/style.scss'],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            open: true,
+            hot: true,
+            port: 5000||process.env.port,
+            writeToDisk:true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.(woff|woff2|ttf|eot)$/,
+                    use: [
+                        {
+                        loader :'file-loader',
+                        options : {
+                            name: '[hash]-[name].[ext]'
+                            
+                        }
+                        }
+                    ]
+                    
+                },
+                {
+                    test: /\.(png|jp(e*)g|svg)$/,  
+                    use: [{
+                        loader: 'url-loader',
+                        options: { 
+                            limit: 8000, // Convert images < 8kb to base64 strings
+                            name: 'images/[hash]-[name].[ext]'
+                        } 
+                    }]
                 }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                  ]
-            },
-            {
-                test: /\.(woff|woff2|ttf|eot)$/,
-                use: [
-                    {
-                     loader :'file-loader',
-                     options : {
-                         name: '[hash]-[name].[ext]'
-                         
-                     }
-                    }
-                ]
                 
-            },
-            {
-                test: /\.(png|jp(e*)g|svg)$/,  
-                use: [{
-                    loader: 'url-loader',
-                    options: { 
-                        limit: 8000, // Convert images < 8kb to base64 strings
-                        name: 'images/[hash]-[name].[ext]'
-                    } 
-                }]
-            }
+            ],
+        },
+        plugins: [
             
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Bagh Chal',
-            template: './src/index.html'
-        }),
-        new CopyPlugin([{
-            from: 'fbapp-config.json',
-            to: 'fbapp-config.json'
-        }])
-      ],    
+            new CleanWebpackPlugin(['dist']),
+            new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({
+                title: 'Bagh Chal',
+                template: './src/index.html',
+                env: env || 'development'
+            }),
+            new CopyPlugin([{
+                from: 'fbapp-config.json',
+                to: 'fbapp-config.json'
+            }])
+        ],  
+    }  
 };
